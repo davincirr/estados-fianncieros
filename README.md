689b2b79d7cd9986eaf1ec86bdda78b45d33a913
home <Leo>
2023-11-08 23:33:28 -0500

    intento 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# estados-fianncieros
+# estados-fianncieros
---
+++ bienvenida.php
@@ -0,0 +1,101 @@
+<?php
+echo
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>interfaz grafica-cubillos solano</title>
+    <link rel="stylesheet" href="stylemenu.css">
+<script src="https://kit.fontawesome.com/41bcea2ae3.js"
+crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+<div class="logo">
+<img src="imagenes/logo.png" alt="">
+</div>
+<div class="header__superior">
+<div class="search">
+<input type="search" placeholder="¿Qué deseas buscar?">
+</div>
+</div>
+<div class="container__menu">
+<div class="menu">
+<input type="checkbox" id="check__menu">
+<label id="#labes__check"for="check__menu">
+</label>
+<nav>
+<ul>
+<li><a href="#"id="selected"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg></a></li>
+<li><a href="#">Mi perfil</a></li>
+<li><a href="#">Servicios</a>
+<ul>
+<li><a href="#">Documentos</a></li>
+<li><a href="#">Estados financieros</a></li>
+<li><a href="#">Cotizaciones</a></li>
+<li><a href="#">Facturas</a></li>
+</ul>
+</li>
+<li><a href="#">Usarios</a></li>
+<li><a href="#">Apoyo</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</header>
+<main>
+<article>
+<h2>Quienes somos.</h2>
+<p>Somos un equipo de profesionales altamente calificados en 
+temas tributarios, contables, de auditoria y legales.con mas de quince años 
+de experiencia en la asesoría de compañías nacionales y extranjeras.</p>
+<p> Basamos la calidad de nuestros servicios en la actualización 
+permanente de los socios y los colaboradores, lo cual nos permite brindar 
+atención personalizada y oportuna a nuestros clientes.</p>
+</article>
+<article>
+<h2>Mision</h2>
+<p>Generar soluciones empresariales de alta calidad, basadas en 
+las necesidades de nuestros clientes y el sector económico en el que se 
+encuentren, mediante un servicio personalizado y confiable.</p>
+</article>
+<article>
+<h2>Vision</h2>
+<p>Ser una firma de Auditoría y Consultoría Empresarial líder a 
+nivel nacional, generando el crecimiento económico constante de 
+nuestros clientes, mediante asesorías de excelente calidad, siempre basadas 
+en la actualización permanente de nuestros socios y colaboradores.
+</p>
+</article>
+<div class="footer">
+<div class="row">
+<div class="col-12 col-lg-6 uno">
+<h1>Datos de contacto</h1>
+<p>PBX 601 9140424 </p>
+<p> contacto@cubillossolanoyasociados.com</p>
+</div>
+<div class="col-12 col-lg-6 dos">
+<h1>Ubicación</h1>
+<p> <strong>Bogotá</strong>: Calle 98 No 21 - 50 torre 
+Empresarial</p>
+<p class="calle">Calle 98 Oficina 902</p>
+<p> <strong>Fusagasugá</strong>: Calle 17 No 12 - 06 Piso 2 
+Oficina 201</p>
+</div>
+</div>
+<div class="privacidad">
+<p class="p_aviso"><span class="aviso-2"> CUBILLOS SOLANO Y 
+ASOCIADOS S.A.S 2023 </span>
+<a href="#" class="aviso"> Aviso legal y Política de 
+privacidad </a>
+<a href="#"class="aviso"> Política de cookies</a></p>
+</div>
+</div>
+</main>
+    
+</body>
+</html>';
+?>
